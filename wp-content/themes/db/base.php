b2a7a139355dae708e8d2898c5d4ff17f8ab6c11
Casper Klenz-Kitenge <hi@cabgfx.com>
2013-06-27 23:13:18 +0200

    Setup LiveReload compiling and reloading

--- wp-content/themes/db/base.php
+++ wp-content/themes/db/base.php
@@ -28,5 +28,9 @@
 
   <?php get_template_part('templates/footer'); ?>
 
+  <?php if (WPBP_ENV == 'development'): ?>
+    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+  <?php endif; ?>
+
 </body>
 </html>
