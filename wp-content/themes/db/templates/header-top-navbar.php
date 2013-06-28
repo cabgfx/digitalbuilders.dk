1cf1ab3af199cc0ac568ed3c7fa2d08aeb68b48b
Casper Klenz-Kitenge <hi@cabgfx.com>
2013-06-28 14:38:42 +0200

    Full chrome styling

--- wp-content/themes/db/templates/header-top-navbar.php
+++ wp-content/themes/db/templates/header-top-navbar.php
@@ -6,9 +6,19 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </a>
+
       <a class="brand" href="<?php echo home_url(); ?>/">
-        <?php bloginfo('name'); ?>
+        <?php
+          /* 
+          *  Digital Builders should be written as two words,
+          *  but we want to massage it a bit in the logo display.
+          *  To become DigitalBuilders and inject some markup for styling.
+          *
+          */
+          echo preg_replace("/^([a-z]+)\s([a-z]+)$/i", "<b>\\1</b>\\2", get_bloginfo('name'));
+        ?>
       </a>
+
       <nav class="nav-main nav-collapse collapse" role="navigation">
         <?php
           if (has_nav_menu('primary_navigation')) :
