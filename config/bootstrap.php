7ffe3d8356bd21e47622abcbe5c08c7adafb9e94
Casper Klenz-Kitenge <hi@cabgfx.com>
2013-06-28 13:57:05 +0200

    Add staging env

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,6 +1,10 @@
 <?php
 if ($_SERVER['REMOTE_ADDR']=='127.0.0.1' || $_SERVER['REMOTE_ADDR']=='10.211.55.3' || ereg("^192", $_SERVER['REMOTE_ADDR']) ) {
     require_once 'environments/development.php';
+
+} elseif ($_SERVER['SERVER_NAME']=='db.dukeux.com') {
+    require_once 'environments/staging.php';    
+
 } else {
     require_once 'environments/production.php';
 }
